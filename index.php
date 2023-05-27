18eddf3e7fe62241e52756be0049a75036b9b49c
npcs <69493073+nexuspcs@users.noreply.github.com>
2023-05-28 09:35:00 +1000

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Online Tutoring Marketplace</title>
     <!-- Add CSS and JS links here -->
 </head>
+
 <body>
     <h1>Online Tutoring Marketplace</h1>
     <form action="index.php" method="post">
@@ -17,35 +19,39 @@
     </form>
 
     <?php
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            // Get the input data from the form
-            $subject = $_POST['subject'];
-            $price = $_POST['price'];
-            $distance = $_POST['distance'];
+    // Always display tutors by default
+    $tutors = array(
+        array('name' => 'person1', 'subject' => 'Math', 'price' => 20, 'distance' => 5),
+        array('name' => 'person2', 'subject' => 'English', 'price' => 25, 'distance' => 10),
+        array('name' => 'person3', 'subject' => 'Coding', 'price' => 30, 'distance' => 15),
+    );
 
-            // Dummy data for tutors
-            $tutors = array(
-                array('name' => 'Alice', 'subject' => 'Math', 'price' => 20, 'distance' => 5),
-                array('name' => 'Bob', 'subject' => 'English', 'price' => 25, 'distance' => 10),
-                array('name' => 'Charlie', 'subject' => 'Coding', 'price' => 30, 'distance' => 15),
-            );
+    // If form is submitted, filter tutors based on input
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Get the input data from the form
+        $subject = $_POST['subject'];
+        $price = $_POST['price'];
+        $distance = $_POST['distance'];
 
-            // Loop through the tutors and display the ones that match the search criteria
-            foreach ($tutors as $tutor) {
-                if ((empty($subject) || strtolower($tutor['subject']) == strtolower($subject)) &&
-                    (empty($price) || $tutor['price'] <= $price) &&
-                    (empty($distance) || $tutor['distance'] <= $distance)) {
-                    echo "<div>";
-                    echo "<h2>" . $tutor['name'] . "</h2>";
-                    echo "<p>Subject: " . $tutor['subject'] . "</p>";
-                    echo "<p>Price: $" . $tutor['price'] . "/hour</p>";
-                    echo "<p>Distance: " . $tutor['distance'] . " kilometres</p>";
-                    echo "</div>";
-                }
-            }
-            
-            
-        }
+        // Filter the tutors array
+        $tutors = array_filter($tutors, function ($tutor) use ($subject, $price, $distance) {
+            return (empty($subject) || strtolower($tutor['subject']) == strtolower($subject)) &&
+                (empty($price) || $tutor['price'] <= $price) &&
+                (empty($distance) || $tutor['distance'] <= $distance);
+        });
+    }
+
+    // Loop through the tutors and display them
+    foreach ($tutors as $tutor) {
+        echo "<div>";
+        echo "<h2>" . $tutor['name'] . "</h2>";
+        echo "<p>Subject: " . $tutor['subject'] . "</p>";
+        echo "<p>Price: $" . $tutor['price'] . "/hour</p>";
+        echo "<p>Distance: " . $tutor['distance'] . " kilometres</p>";
+        echo "</div>";
+    }
+
     ?>
 </body>
+
 </html>
